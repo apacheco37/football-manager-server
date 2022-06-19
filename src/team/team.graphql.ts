048f942bec9c170fefe4da1ccd4c40f85031ab40
Andres Pacheco <apacheco73@hotmail.com>
2022-06-19 20:22:42 -0300

    Add matches field to Team gql type

--- src/team/team.graphql.ts
+++ src/team/team.graphql.ts
@@ -1,11 +1,13 @@
 import { gql } from "apollo-server-core";
 import { Context } from "../core/apolloServer";
+import { TeamWithMatches } from "./team.service";
 
 const typeDefs = gql`
   type Team {
     id: ID!
     name: String!
     players: [Player!]!
+    matches: [Match!]!
   }
 
   extend type Query {
@@ -17,6 +19,11 @@ const resolvers = {
   Query: {
     team: (_parent: never, { id }: { id: string }, { services }: Context) => {
       return services.teamService.getTeam(id);
+    },
+  },
+  Team: {
+    matches: (team: TeamWithMatches, _args: never, { services }: Context) => {
+      return services.teamService.getTeamMatches(team);
     },
   },
 };
--- src/team/team.service.ts
+++ src/team/team.service.ts
@@ -1,17 +1,54 @@
+import { Prisma } from "@prisma/client";
 import { AuthenticationError } from "apollo-server-core";
 
 import { BaseService } from "../core/baseService";
 
+export type TeamWithMatches = Prisma.TeamGetPayload<{
+  include: {
+    homeMatches: true;
+    awayMatches: true;
+  };
+}>;
+
 export class TeamService extends BaseService {
-  async getTeam(id: string) {
+  async getTeam(id: string): Promise<TeamWithMatches | null> {
     if (!this.user) {
       throw new AuthenticationError("User is not authenticated.");
     }
+
+    const matchInclude = {
+      homeTeam: true,
+      awayTeam: true,
+      events: {
+        include: {
+          players: true,
+        },
+      },
+      homeRatings: true,
+      awayRatings: true,
+      lineupPlayers: {
+        include: {
+          player: true,
+        },
+      },
+    };
 
     return this.prismaClient.team.findUnique({
       where: {
         id,
       },
+      include: {
+        homeMatches: {
+          include: matchInclude,
+        },
+        awayMatches: {
+          include: matchInclude,
+        },
+      },
     });
+  }
+
+  getTeamMatches(team: TeamWithMatches) {
+    return [...team.homeMatches, ...team.awayMatches];
   }
 }
